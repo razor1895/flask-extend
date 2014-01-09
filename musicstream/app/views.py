bc500c96f4c08b530afe2a999e8228f7814b3cdd
rsj217 <rsj217@gmail.com>
2014-01-09 12:37:54 +0800

    some test demo

---
+++ musicstream/app/views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from flask import render_template, request, send_from_directory, redirect, url_for
+from werkzeug.utils import secure_filename
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename:
+            # filename = secure_filename(file.filename)
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html', filename=filename)
+    return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)    
